463ca46005a98022f7bcf300960cb78f7f7a5664
3Qax <kuba@towarek.pl>
2019-04-26 08:38:05 +0200

    Fix handling of base64 encoded images in /api/paymentimages

--- skarbnik/serializers.py
+++ skarbnik/serializers.py
@@ -6,12 +6,60 @@ from django.contrib.auth import authenticate, user_logged_in
 from django.contrib.auth.hashers import make_password
 from django.db.models import Sum
 
+class Base64ImageField(serializers.ImageField):
+    """
+    A Django REST framework field for handling image-uploads through raw post data.
+    It uses base64 for encoding and decoding the contents of the file.
+
+    Heavily based on
+    https://github.com/tomchristie/django-rest-framework/pull/1268
+
+    Updated for Django REST framework 3.
+    """
+
+    def to_internal_value(self, data):
+        from django.core.files.base import ContentFile
+        import base64
+        import six
+        import uuid
+
+        # Check if this is a base64 string
+        if isinstance(data, six.string_types):
+            # Check if the base64 string is in the "data:" format
+            if 'data:' in data and ';base64,' in data:
+                # Break out the header from the base64 content
+                header, data = data.split(';base64,')
+
+            # Try to decode the file. Return validation error if it fails.
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
+
+            # Generate file name:
+            file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
+            # Get the file name extension:
+            file_extension = self.get_file_extension(file_name, decoded_file)
+
+            complete_file_name = "%s.%s" % (file_name, file_extension, )
+
+            data = ContentFile(decoded_file, name=complete_file_name)
+
+        return super(Base64ImageField, self).to_internal_value(data)
+
+    def get_file_extension(self, file_name, decoded_file):
+        import imghdr
+
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" else extension
+
+        return extension
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
         fields = ('url', 'id_field', 'name', 'username', 'email', 'role', 'password')
-    
+
     def create(self, validated_data):
         user = models.User.objects.create_user(
             email=validated_data['email'],
@@ -27,9 +75,9 @@ class UserMinInfoSerializer(serializers.ModelSerializer):
    class Meta:
        model = models.User
        fields = ('id_field', 'name')
-       
+
 class TeacherSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = models.User
         fields = ('url', 'id_field', 'name', 'username', 'email', 'role', 'password')
@@ -58,6 +106,7 @@ class ClassMinInfoSerializer(serializers.ModelSerializer):
 
 class PaymentImageSerializer(serializers.ModelSerializer):
     id_field = serializers.IntegerField(source='pk', read_only=True)
+    image = Base64ImageField(max_length=None, use_url=True)
     class Meta:
         model = models.PaymentImage
         fields = ('id_field', 'payment', 'image')
@@ -155,4 +204,3 @@ class JWTSerializer(JSONWebTokenSerializer):
             msg = 'Must include "{username_field}" and "password".'
             msg = msg.format(username_field=self.username_field)
             raise serializers.ValidationError(msg)
-
